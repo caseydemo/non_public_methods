678cc28747b00b3d8e78b8927e05afdf59a32735
Casey DeMattina <cdemattina@gmail.com>
2022-08-23 07:55:41 -0400

    using reflection class to access private methods for unit testing

--- tests/ItemTest.php
+++ tests/ItemTest.php
@@ -11,17 +11,26 @@ class ItemTest extends TestCase
         $this->assertNotEmpty($item->getDescription());                    
     }
     
-    public function testIDisAnInteger()
-    {
-        $item = new ItemChild;
-        
+    public function testIDisAnInteger() {
+        $item = new ItemChild;        
         $this->assertIsInt($item->getID());
     }    
 
-    public function testTokenIsAString()
-    {
-        $item = new ItemChild;
-        
-        $this->assertIsString($item->getToken());        
+    public function testTokenIsAString() {
+        $item = new Item;
+        $reflector = new ReflectionClass(Item::class);
+        $method = $reflector->getMethod('getToken');
+        $method->setAccessible(true);
+        $result = $method->invoke($item);        
+        $this->assertIsString($result);        
     }    
+
+    public function test_prefix_token_starts_with_prefix() {
+        $item = new Item;
+        $reflector = new ReflectionClass(Item::class);
+        $method = $reflector->getMethod('getPrefixedToken');
+        $method->setAccessible(true);
+        $result = $method->invokeArgs($item, ['example']); // two params - first is obj of the class, second is an array of arguments
+        $this->assertStringStartsWith('example', $result);
+    }
 }
